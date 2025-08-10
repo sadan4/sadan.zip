178c3745a773993e4151d8be21ee0afd1c20927d
sadan4 <117494111+sadan4@users.noreply.github.com>
2025-08-10 22:01:04 +0000

    why did i do this

--- src/App.tsx
+++ src/App.tsx
@@ -13,52 +13,30 @@ import cn from "./utils/cn";
 function Links() {
     return (
         <div className="flex [&_svg]:h-14 gap-3 [&_svg]:text-secondary">
-            {
-                [
-                    (props) => (
-                        <DiscordIconLink
-                            userId="521819891141967883"
-                            key="521819891141967883"
-                            {...props}
-                        />
-                    ),
-                    (props) => (
-                        <NameMCIconLink
-                            UUID="b7c4f5b1-762f-41ea-b6b4-45aba74198e5"
-                            key="b7c4f5b1-762f-41ea-b6b4-45aba74198e5"
-                            {...props}
-                        />
-                    ),
-                    (props) => (
-                        <LastFMIconLink
-                            username="sadan4"
-                            key="lastfm-sadan4"
-                            {...props}
-                        />
-                    ),
-                    (props) => (
-                        <SteamIconLink
-                            userId="sadan4"
-                            key="steam-sadan4"
-                            {...props}
-                        />
-                    ),
-                    (props) => (
-                        <FortniteDBIconLink
-                            username="sadan4"
-                            key="fndb-sadan4"
-                            {...props}
-                        />
-                    ),
-                    (props) => (
-                        <GithubIconLink
-                            username="sadan4"
-                            key="gh-sadan4"
-                            {...props}
-                        />
-                    ),
-                ].map((el, _idx) => el({}))
-            }
+            <DiscordIconLink
+                userId="521819891141967883"
+                key="521819891141967883"
+            />
+            <NameMCIconLink
+                UUID="b7c4f5b1-762f-41ea-b6b4-45aba74198e5"
+                key="b7c4f5b1-762f-41ea-b6b4-45aba74198e5"
+            />
+            <LastFMIconLink
+                username="sadan4"
+                key="lastfm-sadan4"
+            />
+            <SteamIconLink
+                userId="sadan4"
+                key="steam-sadan4"
+            />
+            <FortniteDBIconLink
+                username="sadan4"
+                key="fndb-sadan4"
+            />
+            <GithubIconLink
+                username="sadan4"
+                key="gh-sadan4"
+            />
         </div>
     );
 }
