1b9bd2a15fa48465c6c4add4d9d82383aef16b6d
sadan <117494111+sadan4@users.noreply.github.com>
2025-08-20 18:44:19 -0400

    use declarative route managment

--- src/main.tsx
+++ src/main.tsx
@@ -1,54 +1,52 @@
 import { installF8Break } from "@/utils/devtools";
 
 import "./index.css";
-import { StrictMode } from "react";
+import { lazy, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 installF8Break();
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        HydrateFallback: () => null,
-        children: [
-            {
-                index: true,
-                async lazy() {
-                    const Component = (await import("./pages")).default;
-
-                    return {
-                        Component,
-                    };
-                },
-            },
-            {
-                path: "demangler",
-                async lazy() {
-                    const Component = (await import("./pages/demangler")).default;
-
-                    return {
-                        Component,
-                    };
-                },
-            },
-            {
-                path: "minky",
-                async lazy() {
-                    const Component = (await import("./pages/minky")).default;
-
-                    return {
-                        Component,
-                    };
-                },
-            },
-        ],
+const routes = {
+    index: lazy(() => import("./pages")),
+    demangler: {
+        index: lazy(() => import("./pages/demangler")),
     },
-]);
+    minky: {
+        index: lazy(() => import("./pages/minky")),
+    },
+    components: {
+        index: lazy(() => import("./pages/components")),
+    },
+};
 
 createRoot(document.body)
     .render((
         <StrictMode>
-            <RouterProvider router={router} />
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        HydrateFallback={() => null}
+                    >
+                        <Route
+                            index
+                            element={<routes.index />}
+                        />
+                        <Route
+                            path="demangler"
+                            element={<routes.demangler.index />}
+                        />
+                        <Route
+                            path="minky"
+                            element={<routes.minky.index />}
+                        />
+                        <Route
+                            path="components"
+                            element={<routes.components.index />}
+                        />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </StrictMode>
     ));
