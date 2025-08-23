a4632863cd0105916ef345a11e3b49752d3327f9
sadan <117494111+sadan4@users.noreply.github.com>
2025-08-23 01:45:00 -0400

    fix formatting

--- src/components/icons/ErrorIcon.tsx
+++ src/components/icons/ErrorIcon.tsx
@@ -1,6 +1,6 @@
 import type { ComponentProps, FC } from "react";
 
-export const ErrorIcon: FC<ComponentProps<"svg">> = ({width = 24, height = 24, ...props}) => (
+export const ErrorIcon: FC<ComponentProps<"svg">> = ({ width = 24, height = 24, ...props }) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 36 36"
