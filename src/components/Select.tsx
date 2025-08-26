cb34501a20eae6806254e352837754bc33aed78b
sadan <117494111+sadan4@users.noreply.github.com>
2025-08-26 17:34:32 -0400

    horror

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -7,6 +7,8 @@ interface SelectContext {
 const SelectContext = createContext<SelectContext | null>(null);
 
 export interface SelectProps extends PropsWithChildren {
+    initialVisibility?: boolean;
+    visible?: boolean;
 }
 
 export function Select({}: SelectProps) {
--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,3 +1,7 @@
 export function clamp(min: number, max: number, value: number): number {
     return Math.min(Math.max(value, min), max);
 }
+
+export function isNaN(value: number): boolean {
+    return Number.isNaN(value);
+}
