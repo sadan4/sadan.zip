ad0042319e44525072407383c0a3a78aeee263b0
Indiana Jone (Indi) <150982280+KrstlSkll69@users.noreply.github.com>
2025-09-14 19:03:47 -0400

    Update friends.ts

--- src/utils/friends.ts
+++ src/utils/friends.ts
@@ -59,4 +59,10 @@ export const friends: Friend[] = [
         url: new URL("https://wingio.xyz/"),
         avatarUrl: new URL("https://avatars.githubusercontent.com/u/44992537?v=4"),
     },
+    {
+        name: "krstlskll",
+        discordId: "929208515883569182",
+        url: new URL("https://krstlskll69.github.io/"),
+        avatarUrl: new URL("https://krstlskll69.github.io/assets/images/brand/7a394673-1c73-4099-8244-4d3c0f416cbd.png"),
+    },
 ];
