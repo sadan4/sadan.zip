0a0857221d0972be51d5c3b00cbaefed86b5e445
sadan <117494111+sadan4@users.noreply.github.com>
2025-10-22 02:01:56 -0400

    fix elissa

--- src/utils/friends.ts
+++ src/utils/friends.ts
@@ -33,8 +33,9 @@ export const friends: Friend[] = [
     {
         name: "elissa",
         discordId: "381494697073573899",
-        // url: new URL("https://elissa.moe"),
+        url: new URL("https://elissa.moe"),
         avatarUrl: new URL("https://avatars.githubusercontent.com/u/140089641?v=4"),
+        _88x31url: new URL("https://elissa.moe/"),
     },
     {
         name: "sqaaakoi",
