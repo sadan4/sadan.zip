6a086e248ba6b7cbff9360eddd64b27f354a0072
sadan <117494111+sadan4@users.noreply.github.com>
2025-10-29 23:58:06 -0400

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -17,63 +17,12 @@ export default defineConfig({
     plugins: [
         react({
             babel: {
-                plugins: [
-                    [
-                        "babel-plugin-react-compiler",
-                        {
-                            logger: {
-                                logEvent(filename: any, event: {
-                                    kind: string;
-                                    detail: {
-                                        reason: any;
-                                        description: any;
-                                        loc: {
-                                            start: {
-                                                line: any;
-                                                column: any;
-                                            };
-                                        };
-                                        suggestions: any;
-                                    };
-                                }) {
-                                    return;
-                                    if (event.kind === "CompileError") {
-                                        console.error(`\nCompilation failed: ${filename}`);
-                                        console.error(`Reason: ${event.detail.reason}`);
-                                        if (event.detail.description) {
-                                            console.error(`Details: ${event.detail.description}`);
-                                        }
-                                        if (event.detail.loc) {
-                                            const {
-                                                line,
-                                                column,
-                                            } = event.detail.loc.start ?? {};
-
-                                            console.error(`Location: Line ${line}, Column ${column}`);
-                                        }
-                                        if (event.detail.suggestions) {
-                                            console.error("Suggestions:", event.detail.suggestions);
-                                        }
-                                    }
-                                },
-                            },
-                        },
-                    ],
-                ],
+                plugins: ["babel-plugin-react-compiler"],
             },
         }),
         tailwindcss(),
         tsconfigPaths(),
         devtoolsJson(),
-        {
-            name: "log-server-requests",
-            configureServer(server) {
-                server.middlewares.use((req, _res, next) => {
-                    console.log(`[Vite Dev Server] ${req.method} ${req.url}`);
-                    next();
-                });
-            },
-        },
     ],
     build: {
         sourcemap: true,
