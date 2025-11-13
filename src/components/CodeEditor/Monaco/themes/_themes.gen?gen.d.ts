b368538b34473b71c545576d52e816c621718720
sadan4 <117494111+sadan4@users.noreply.github.com>
2025-11-13 09:51:28 -0500

    Update and rename _themes.gen?gen.d.ts to thanks_windows

--- src/components/CodeEditor/Monaco/themes/_themes.gen?gen.d.ts
+++
@@ -1,4 +0,0 @@
-import * as monaco from "monaco-editor";
-export declare const TokyoNight: monaco.editor.IStandaloneThemeData;
-export declare const TokyoNightLight: monaco.editor.IStandaloneThemeData;
-export declare const TokyoNightStorm: monaco.editor.IStandaloneThemeData;
