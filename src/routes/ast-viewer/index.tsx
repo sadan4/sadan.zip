cc4faf1f3a967bfe67fd937721934cc6f130348f
sadan <117494111+sadan4@users.noreply.github.com>
2026-02-11 19:24:24 -0500

    fix require

--- src/routes/ast-viewer/index.tsx
+++ src/routes/ast-viewer/index.tsx
@@ -1,7 +1,7 @@
 import { unavailableImport } from "@/utils/error";
 import { createFileRoute } from "@tanstack/react-router";
 
-const ui = import.meta.env.SSR ? unavailableImport("@/components/ASTViewer") : require("@/components/ASTViewer") as typeof import("@/components/ASTViewer");
+const ui = import.meta.env.SSR ? unavailableImport("@/components/ASTViewer") : await import("@/components/ASTViewer");
 
 export const Route = createFileRoute("/ast-viewer/")({
     component: RouteComponent,
