b77911413e3c8d53d4ae9fd9cbfe8c659b85b2f0
sadan <117494111+sadan4@users.noreply.github.com>
2026-02-26 18:24:35 -0500

    fix merge issue

--- src/hooks/rect.ts
+++ src/hooks/rect.ts
@@ -240,8 +240,6 @@ export function useFragmentRect<T extends keyof DOMRect>(
             return picked;
         });
     }, [mapper]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [mapper, ...extraDeps]);
 
     function updateSizeIfNeeded() {
         const f = fragmentRef.current;
@@ -267,7 +265,6 @@ export function useFragmentRect<T extends keyof DOMRect>(
         }
         // eslint-disable-next-line @eslint-react/exhaustive-deps
     }, [fragmentRef, setSize, ...extraDeps]);
-    }, [fragmentRef, setSize]);
 
     useEventHandler("scrollend", updateSizeIfNeeded, scroller);
     useEventHandler("scrollend", updateSizeIfNeeded);
