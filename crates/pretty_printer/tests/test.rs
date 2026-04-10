9e8d285dc8c5c6cfe19db7667514d2f10c15d479
sadan <117494111+sadan4@users.noreply.github.com>
2026-04-10 00:24:53 -0400

    work

--- crates/pretty_printer/tests/test.rs
+++ crates/pretty_printer/tests/test.rs
@@ -1,4 +1,5 @@
 #![allow(clippy::needless_raw_string_hashes)]
+//! ported from <https://github.com/ChromeDevTools/devtools-frontend/blob/main/front_end/entrypoints/formatter_worker/JavaScriptFormatter.test.ts>
 use insta::assert_snapshot;
 use macros::test;
 use pretty_printer::{format, format2};
@@ -17,5 +18,22 @@ fn async_function_expressions() {
 	async function foo() {
 	  return await Promise.resolve(1);
 	}
+	");
+}
+
+#[test]
+fn top_level_await() {
+	let source = "const myFile=await import(\n\"my-file.mjs\");";
+	let out = format2(source).unwrap();
+	assert_snapshot!(out, @r#"const myFile = await import("my-file.mjs");"#);
+}
+
+#[test]
+fn idents_with_escaped_characters() {
+	let source = r#"const x=42;let \u0275_escaped;"#;
+	let out = format2(source).unwrap();
+	assert_snapshot!(out, @r"
+	const x = 42;
+	let \u0275_escaped;
 	");
 }
