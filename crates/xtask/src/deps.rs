c947cc6cf97c908aaee341a8d1babe82767d5340
sadan <117494111+sadan4@users.noreply.github.com>
2026-04-16 13:36:25 -0400

    add workflow to build site

--- crates/xtask/src/deps.rs
+++ crates/xtask/src/deps.rs
@@ -1,11 +1,14 @@
-use std::process;
+use std::{env, process};
 
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
 use tracing::debug;
 
 use crate::util::cmd::{CommandExt, resolve_program_in_path};
 
 pub fn pnpm_i() -> Result<()> {
+	if env::var("CI").is_ok() {
+		bail!("auto pnpm install is not supported in CI");
+	}
 	let pnpm_path = resolve_program_in_path("pnpm").context("Failed to find pnpm in PATH")?;
 	debug!("Found pnpm at {pnpm_path:?}");
 	process::Command::new(pnpm_path)
