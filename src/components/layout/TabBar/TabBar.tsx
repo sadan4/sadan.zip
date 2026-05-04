097200c97538f14c90413a1586d69ae5f6428a37
sadan4 <117494111+sadan4@users.noreply.github.com>
2026-05-04 02:31:07 -0400

    use css anchors for TabBar (#62)

--- src/components/layout/TabBar/TabBar.tsx
+++ src/components/layout/TabBar/TabBar.tsx
@@ -3,21 +3,14 @@ import { AnimateHeight } from "@/components/effects/AnimateHeight";
 import { Box } from "@/components/layout/Box";
 import { VerticalLine } from "@/components/Lines/VerticalLine";
 import { Text } from "@/components/Text";
-import { useForceUpdater } from "@/hooks/forceUpdater";
-import { useImperativeSprings } from "@/hooks/imperativeSprings";
-import { useRect } from "@/hooks/rect";
-import { useResizeObserverFromRef } from "@/hooks/resizeObserver";
 import { joinWithKey } from "@/utils/array";
 import cn from "@/utils/cn";
-import { measureRect } from "@/utils/dom/rect";
 import { assert } from "@/utils/error";
-import { updateRef } from "@/utils/ref";
-import { animated } from "@react-spring/web";
 
 import { TabBarPosition } from "./enum";
 import * as styles from "./styles.module.scss";
 
-import { type ReactNode, type RefCallback, useCallback, useEffect, useRef, useState } from "react";
+import { type ReactNode, useEffect, useState } from "react";
 
 export interface TabRowItemProps {
     isSelected: boolean;
@@ -78,7 +71,6 @@ interface TabButtonProps {
     tab: Tab;
     activeTabId: string;
     className?: string;
-    setActiveTabRef: RefCallback<HTMLElement>;
     setActiveTab: (tabId: string) => void;
     onTabChange?: (tab: Tab) => void;
     isManaged: boolean;
@@ -88,25 +80,15 @@ function TabButton({
     tab: tabProp,
     activeTabId,
     className,
-    setActiveTabRef,
     setActiveTab,
     onTabChange,
     isManaged,
 }: TabButtonProps) {
     const isActive = tabProp.id === activeTabId;
-    const ref = useRef<HTMLDivElement>(null);
-
-
-    const setRef = useCallback((node: HTMLDivElement | null) => {
-        updateRef(ref, node);
-        if (isActive && node) {
-            updateRef(setActiveTabRef, node);
-        }
-    }, [isActive, setActiveTabRef]);
 
     return (
         <Clickable
-            className={cn(className)}
+            className={cn(className, isActive && styles.selectedTab)}
             onClick={() => {
                 const isNew = tabProp.id !== activeTabId;
 
@@ -114,13 +96,9 @@ function TabButton({
                     setActiveTab(tabProp.id);
                 }
                 if (isNew) {
-                    if (!isManaged) {
-                        setActiveTabRef(ref.current);
-                    }
                     onTabChange?.(tabProp);
                 }
             }}
-            ref={setRef}
         >
             {tabProp.renderTab({
                 isSelected: isActive,
@@ -144,11 +122,7 @@ export function TabBar({
     assert(!(selectedTab && initialSelectedTab), "You can only provide one of selectedTab or initialSelectedTab");
 
     const [tab, setTab] = useState(selectedTab ?? initialSelectedTab ?? tabs[0]?.id ?? "");
-    const [activeTab, setActiveTab] = useState<HTMLElement | null>(null);
-    const [layoutMarkerDep, forceLayoutMarker] = useForceUpdater();
-    const activeRect = useRect(activeTab);
     const isManaged = selectedTab !== undefined;
-    const tabsContainerRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         if (selectedTab) {
@@ -158,37 +132,9 @@ export function TabBar({
 
     const selectedTabObj = tabs.find(({ id }) => id === tab) ?? fallbackTab;
 
-    const { x, y, width } = useImperativeSprings({
-        x: 0,
-        y: 0,
-        width: 0,
-    });
-
-    useResizeObserverFromRef(tabsContainerRef, forceLayoutMarker);
-
-    useEffect(() => {
-        if (activeRect) {
-            const size = activeRect;
-
-            if (x.get() === 0) {
-                // FIXME: horrible hack for a bug that i dont even know the cause of
-                const size = measureRect(activeTab!);
-
-                width.set(size.width);
-                y.set(size.y + size.height);
-                x.set(size.x);
-            } else {
-                width.start(size.width);
-                y.start(size.y + size.height);
-                x.start(size.x);
-            }
-        }
-    }, [activeRect, activeTab, layoutMarkerDep, width, x, y]);
-
     return (
         <div className={cn(styles.tabBar, className)}>
             <div
-                ref={tabsContainerRef}
                 className={cn(styles.tabs, positionClasses[tabsPosition], tabsClassName)}
             >
                 {joinWithKey(tabs.map((t) => (
@@ -199,17 +145,9 @@ export function TabBar({
                         setActiveTab={setTab}
                         onTabChange={onTabChange}
                         isManaged={isManaged}
-                        setActiveTabRef={setActiveTab}
                     />
                 )), (i) => (noSeparators ? null : <VerticalLine key={`vl-${i}`} />))}
-                <animated.div
-                    className={styles.marker}
-                    style={{
-                        x,
-                        width,
-                        y,
-                    }}
-                />
+                <div className={styles.marker} />
             </div>
             <Box
                 className={cn(styles.content, contentClassName)}
