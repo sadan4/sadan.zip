ee8b934eed3e4b870d86219db7f742fe09c79081
sadan <117494111+sadan4@users.noreply.github.com>
2026-09-17 11:27:15 -0400

    fix bug

--- crates/companion_lsp/src/util/str_util.rs
+++ crates/companion_lsp/src/util/str_util.rs
@@ -2,5 +2,5 @@ pub fn offset_into(whole: &[u8], sub: &[u8]) -> Option<usize> {
 	whole
 		.as_ptr_range()
 		.contains(&sub.as_ptr())
-		.then(|| whole.as_ptr() as usize - sub.as_ptr() as usize)
+		.then(|| sub.as_ptr().addr() - whole.as_ptr().addr())
 }
